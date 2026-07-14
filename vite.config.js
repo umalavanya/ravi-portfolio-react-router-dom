c1a08f7469a88053b2ad6449b30be3d2d349fa04
umalavanya <umalavanya7@gmail.com>
2026-07-14 00:41:27 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: 'ravi-portfolio-react-router-dom'
-})
+export default defineConfig(({ command, mode }) => {
+  // 'command' → 'build' or 'serve'
+  // 'mode' → 'production' or 'development'
+  
+  const isProduction = mode === 'production';
+  
+  return {
+    plugins: [react()],
+    // Production (GitHub Pages) కోసం project name, Development కోసం '/'
+    base: isProduction ? '/ravi-portfolio-react-router-dom/' : '/',
+  }
+}) ;
